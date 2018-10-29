7c6f5329cde919a53cec84b8cc8ddbb8142a8699
taro.tanaka <reon09032@gmail.com>
2018-10-29 11:49:13 +0000

    have fix

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -2,10 +2,12 @@ class RankingsController < ApplicationController
   def want
     @ranking_counts = Want.ranking
     @items = Item.find(@ranking_counts.keys)
+    @count = 'Want'
   end
   
   def have
     @ranking_counts = Have.ranking
     @items = Item.find(@ranking_counts.keys)
+    @count = 'Have'
   end
 end
